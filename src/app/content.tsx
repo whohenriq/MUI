52a6285e3a79b745924bd4dc9de2c367a1a334c6
Henrique Eduardo <heduardocr7@gmail.com>
2025-05-23 10:20:48 -0300

    style: adjust modal style

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -40,24 +40,55 @@ export function Content() {
         onOpenModal={handleOpenModal}
       />
 
-      <Dialog open={open} onClose={handleCloseModal}>
+      <Dialog
+        open={open}
+        onClose={handleCloseModal}
+        PaperProps={{
+          sx: { borderRadius: 3, p: 2, minWidth: 350 },
+        }}
+      >
         <DialogTitle>
           {music.title} - {music.artist}
         </DialogTitle>
+
         <DialogContent>
-          <Typography gutterBottom>Álbum: {music.album}</Typography>
-          <Typography gutterBottom>{music.description}</Typography>
-          <Button
-            href={music.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            variant="contained"
-            sx={{ mt: 2 }}
-          >
-            🔗 Ouvir no Spotify
-          </Button>
+          <Box className="flex flex-col items-start">
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              textAlign="center"
+              mb={1}
+            >
+              Álbum: {music.album}
+            </Typography>
+
+            <Typography variant="body2" textAlign="center" mb={2}>
+              {music.description}
+            </Typography>
+          </Box>
+
+          <Box display="flex" justifyContent="center">
+            <Button
+              href={music.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="contained"
+              color="success"
+              startIcon={
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
+                  alt="Spotify"
+                  width={20}
+                  height={20}
+                />
+              }
+            >
+              Ouvir no Spotify
+            </Button>
+          </Box>
         </DialogContent>
-        <DialogActions>
+
+        <DialogActions sx={{ justifyContent: "center" }}>
           <Button onClick={handleCloseModal}>Fechar</Button>
         </DialogActions>
       </Dialog>
